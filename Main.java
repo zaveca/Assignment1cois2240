1e63af7cffb119c21a8a5f47a9b2f084b5578042
zaveca <zaveca@users.noreply.github.com>
2019-02-04 17:00:37 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Fibonacci Sequence program. ");
@@ -9,33 +10,31 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         long start = System.nanoTime();
-        System.out.println("The fibonacci series of numbers calculated to " + number + " iteratively are: ");
+        System.out.println("The fibonacci series of numbers calculated to " + number + " recursively are: ");
         for (int i = 0; i <= number - 1; i++) {
-            System.out.print(fiboSeriesIte(i) + " ");
+            System.out.print(fiboSeriesRec(i) + " ");
         }
         long end = System.nanoTime();
+        System.out.println("Time taken for recursive method (in nanoseconds)is: "+(end-start));
 
-        System.out.println("Time taken for iterative method (in nanoseconds): "+(end-start));
     }
 
-    //iterative function
-    public static int fiboSeriesIte(int number) {
-        if (number == 0) {
-            return 0;
-        }
-        {
+    //recursive function
+
+        public static int fiboSeriesRec(int number) {
+                if (number == 0) {
+                return 0;
+
+            }
+                {
             if (number == 1 || number == 2) {
                 return 1;
             }
-            int fn1 = 1, fn2 = 1, fibsum = 1;
-            for (int i = 3; i <= number; i++) {
-                fibsum = fn1 + fn2;
-                fn1 = fn2;
-                fn2 = fibsum;
-            }
-            return fibsum;
+            return fiboSeriesRec(number - 1) + fiboSeriesRec(number - 2);
         }
     }
 }
+
+
 
 
